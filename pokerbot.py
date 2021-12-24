85c05fdea0261e566ebacb56500d3fd62acde270
Dave Niewinski <davesarmoury@gmail.com>
2021-12-24 11:27:29 -0500

    Added flask and cleaned audio listing

--- pokerbot.py
+++ pokerbot.py
@@ -1,53 +1,26 @@
 #!/usr/bin/env python3
 
+import sys
 from pyniryo import *
 from threading import Thread, Lock
 import pygame
 import time
 import random
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 robot_ip = "192.168.2.114"
 grip_speed = 500
-
-audio = {}
-audio["startup"] = []
-audio["startup"].append("turret_active_1.wav")
-audio["startup"].append("turret_active_5.wav")
-audio["startup"].append("turret_autosearch_1.wav")
-audio["startup"].append("turret_pickup_2.wav")
-
-audio["bet"] = []
-audio["bet"].append("turret_active_3.wav")
-audio["bet"].append("turret_deploy_3.wav")
-audio["bet"].append("turret_disabled_1.wav")
-audio["bet"].append("turret_pickup_2.wav")
-
-audio["fold"] = []
-audio["fold"].append("turret_disabled_3.wav")
-audio["fold"].append("turret_disabled_1.wav")
-audio["fold"].append("turret_disabled_4.wav")
-audio["fold"].append("turret_pickup_10.wav")
-audio["fold"].append("turret_retire_6.wav")
-audio["fold"].append("turret_retire_7.wav")
-audio["fold"].append("turretshotbylaser10.wav")
-
-audio["win"] = []
-audio["win"].append("turret_active_6.wav")
-audio["win"].append("turret_disabled_1.wav")
-audio["win"].append("turret_pickup_2.wav")
-audio["win"].append("turret_retire_3.wav")
-audio["win"].append("turret_retire_3.wav")
-audio["win"].append("turret_retire_3.wav")
-
-audio["lose"] = []
-audio["lose"].append("turret_disabled_5.wav")
-audio["lose"].append("turret_disabled_6.wav")
-audio["lose"].append("turret_disabled_7.wav")
-audio["lose"].append("turret_disabled_8.wav")
-audio["lose"].append("turretshotbylaser09.wav")
-audio["lose"].append("turretshotbylaser10.wav")
-audio["lose"].append("turretwitnessdeath02.wav")
-audio["lose"].append("turretwitnessdeath10.wav")
 
 def playAudio(audio, type, dir="portal_turret_audio/audio/", odds=70):
     randval = random.randint(0, 100)
@@ -79,7 +52,7 @@ def initRobot(ip):
         return None
 
 def cameraThread():
-    global keep_camming, lock, new_frame, _frame
+    global shut_r_down, image_lock, new_frame, _frame
 
     w = 1280
     h = 720
@@ -91,15 +64,53 @@ def cameraThread():
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     writer = cv2.VideoWriter("/home/davesarmoury/poker_video.mp4", fourcc, 30, (w, h))
 
-    while keep_camming:
+    while not shut_r_down:
         ret_val, frame = cam.read()
-        lock.acquire()
+        image_lock.acquire()
         _frame = frame
         new_frame = True
-        lock.release()
+        image_lock.release()
         writer.write(frame)
+        cv2.imshow('Poker Cam', _frame)
+        cv2.waitKey(1)
 
+    print("Closing video stream...")
     writer.release()
+    print("Video Stream Closed")
+
+def armThread():
+    global shut_r_down, robot, current_bet, new_turn, new_deal
+
+    while not shut_r_down:
+        #ROBOTTING
+        pass
+
+    print("Disconnecting Robot...")
+    robot.close_connection()
+    print("Robot Disconnected")
+
+@app.route("/quit")
+def die():
+    global shut_r_down
+    print("Shutting Down...")
+    shut_r_down = True
+    pygame.mixer.quit()
+    time.sleep(1.0)
+    print("Goodnight")
+    sys.exit(0)
+
+@app.route("/deal")
+def new_deal():
+    global new_deal
+    new_deal = True
+    return "Confirmed"
+
+@app.route("/turn")
+def new_turn():
+    global current_bet, new_turn
+    current_bet = int(request.args.get("current_bet"))
+    new_turn = True
+    return "Confirmed"
 
 def bet(robot):
     pass
@@ -117,47 +128,43 @@ def river(robot):
     pass
 
 def getFrame():
-    global lock, _frame, new_frame
-    lock.acquire()
+    global image_lock, _frame, new_frame
+    image_lock.acquire()
     new_frame = False
     rframe = _frame.copy()
-    lock.release()
+    image_lock.release()
 
     return rframe
 
 def main():
-    global keep_camming, lock, new_frame, audio
+    global shut_r_down, image_lock, new_frame, robot
+
+    new_frame = False
+    shut_r_down = False
+
+    audioFile = open("audio.yaml", 'r')
+    audio = load(audioFile, Loader=Loader)
+    audioFile.close()
 
     robot = initRobot(robot_ip)
 
     if robot:
         pygame.mixer.init()
+
         openGripper(robot)
         closeGripper(robot)
 
-        new_frame = False
-        keep_camming = True
-        lock = Lock()
+        image_lock = Lock()
         cam_thread = Thread(target=cameraThread, args=())
         cam_thread.daemon = True
         cam_thread.start()
 
+        arm_thread = Thread(target=armThread, args=())
+        arm_thread.daemon = True
+        arm_thread.start()
+
         playAudio(audio, "startup", odds=100)
 
-        while True:
-            if new_frame:
-                img = getFrame()
-                cv2.imshow('Poker Cam', img)
-                if cv2.waitKey(1) == 27:
-                    break  # esc to quit
-            time.sleep(0.01)
-
-        keep_camming = False
-        pygame.mixer.quit()
-        cv2.destroyAllWindows()
-        robot.close_connection()
-        time.sleep(1.0)
-        print("Goodnight")
+        app.run("0.0.0.0")
 
 main()
-
